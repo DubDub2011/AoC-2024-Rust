2578be4a26d48e1e8860efc6d6f439a29864d79a
Owen Watkins <owen.watkins10@outlook.com>
2024-12-08 01:40:39 +0000

    day 1 - read inputs into vec

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,6 +3,24 @@ use std::fs;
 fn main() {
     let file_path = "input.txt";
     let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
+    println!("File contents: {}", contents);
 
-    println!("With text:\n{contents}")
+    let contents: Vec<&str> = contents.split('\n').collect();
+    let mut inputs:Vec<Input> = Vec::new();
+    for line in contents {
+        if line == "" {
+            continue
+        }
+
+        let parts: Vec<&str> = line.split("   ").collect();
+        inputs.push(Input(parts[0].parse().unwrap(), parts[1].parse().unwrap()));
+    }
+
+    for inp in inputs {
+        println!("Input: {}, {}", inp.0, inp.1)
+    }
+
 }
+
+
+struct Input(i32, i32);
