7845a384d3732afa1d804ffeabc92f0e9ca46d04
Owen Watkins <owen.watkins10@outlook.com>
2024-12-08 01:17:56 +0000

    day 1 - read txt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "input.txt";
+    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
+
+    println!("With text:\n{contents}")
 }
