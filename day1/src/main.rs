e349b66e374025204355d063da8e1a3ac0f85ba5
Owen Watkins <owen.watkins10@outlook.com>
2024-12-12 13:12:54 +0000

    day 1 - add part 2 solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,33 +1,34 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let file_path = "input.txt";
     let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
 
     let contents: Vec<&str> = contents.split('\n').collect();
-    let mut inputs:Vec<Input> = Vec::new();
+    let mut left:Vec<i32> = Vec::new();
+    let mut right:Vec<i32> = Vec::new();
+
     for line in contents {
         if line == "" {
             continue
         }
 
         let parts: Vec<&str> = line.split("   ").collect();
-        inputs.push(Input(parts[0].parse().unwrap(), parts[1].parse().unwrap()));
+        left.push(parts[0].parse().unwrap());
+        right.push(parts[1].parse().unwrap());
     }
 
-    println!("Part 1: {}", part1(inputs))
+    let left = &left;
+    let right = &right;
 
+    println!("Part 1: {}", diff_check(left, right));
+    println!("Part 2: {}", similarity(left, right));
 }
 
-fn part1(mut inputs: Vec<Input>) -> i32 {
-    let mut left: Vec<i32> = Vec::new();
-    let mut right: Vec<i32> = Vec::new();
-
-    for inp in inputs {
-        left.push(inp.0);
-        right.push(inp.1);
-    }
-    
+fn diff_check(left: &Vec<i32>, right: &Vec<i32>) -> i32 {   
+    let mut left = left.clone();
+    let mut right = right.clone();
     left.sort();
     right.sort();
 
@@ -39,4 +40,26 @@ fn part1(mut inputs: Vec<Input>) -> i32 {
     total
 }
 
-struct Input(i32, i32);
+fn similarity(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    let mut index = HashMap::new();
+
+    for idx in 0..right.len() {
+        let similarity_value = right[idx];
+        match index.get(&similarity_value) {
+            Some(new_value) => { index.insert(similarity_value, new_value + 1); }
+            None => { index.insert(similarity_value, 1);}
+        }
+    }
+
+    let mut sum = 0;
+    for idx in 0..left.len() {
+        let val = left[idx];
+        match index.get(&val) {
+            Some(count) => { sum = sum + count * val;}
+            None => ()
+        }
+    }
+
+    sum
+}
+
